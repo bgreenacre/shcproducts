f97b85c61120287d5fa5307ef99843ecffc632b9
greenacre <bgree53@searshc.com>
2011-06-16 16:19:26 -0500

    Moved api into Library classes.

---
+++ application/classes/library/api/product.php
@@ -0,0 +1,19 @@
+<?php defined('SHCP_PATH') OR die('No direct script access.');
+/**
+ * shcproducts
+ *
+ * @author Brian Greenacre and Kyla Klein
+ * @email bgreenacre42@gmail.com
+ * @version $Id$
+ * @since Thu 16 Jun 2011 11:34:46 AM
+ */
+
+// -----------------------------------------------------------------------------
+
+/**
+ * SHCP_Library_Api_Search
+ *
+ */
+class SHCP_Library_Api_Product extends SHCP_Library_Api {
+}
+
