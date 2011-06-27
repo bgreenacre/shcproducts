5886f28106b2585646f0d2f1993bfb27bf91e726
Kyla Klein <kylaklein@gmail.com>
2011-06-27 15:00:57 -0500

    [shcproducts] Kyla: added MyGofer as a store choice on the options page

--- application/views/fields/store.php
+++ application/views/fields/store.php
@@ -4,6 +4,8 @@
     <input id="store_sears" name="<?php echo $name; ?>" type="radio" value="Sears" <?php echo ($value == "Sears") ? ' checked="checked"' : ''; ?> />
     <label for="store_sears">Sears</label>  		  
     <input id="store_kmart" name="<?php echo $name; ?>" type="radio" value="Kmart" <?php echo ($value == "Kmart") ? ' checked="checked"' : ''; ?> />
-    <label for="store_kmart">Kmart</label>    		  
+    <label for="store_kmart">Kmart</label>    
+    <input id="store_mygofer" name="<?php echo $name; ?>" type="radio" value="MyGofer" <?php echo ($value == "MyGofer") ? ' checked="checked"' : ''; ?> />
+    <label for="store_mygofer">MyGofer</label>    		  
   </td>
 </tr>
