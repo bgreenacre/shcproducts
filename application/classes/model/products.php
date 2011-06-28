6266ca6e794af3f6b3719ffd8798ef98caed0443
greenacre <bgree53@searshc.com>
2011-06-28 11:33:47 -0500

    Adding in the products model.

---
+++ application/classes/model/products.php
@@ -0,0 +1,24 @@
+<?php defined('SHCP_PATH') OR die('No direct script access.');
+/**
+ * shcproducts
+ *
+ * @author Brian Greenacre and Kyla Klein
+ * @email bgreenacre42@gmail.com
+ * @version $Id$
+ * @since Wed 15 Jun 2011 07:32:09 PM
+ */
+
+// ----------------------------------------------------------------------------
+
+/**
+ * Model_SHCP
+ *
+ */
+class Model_Products extends Model_SHCP {
+
+    public function __construct()
+    {
+        $this->param('post_type', 'shcproduct');
+    }
+
+}
