2ea121e2aa0c68531e2bea80fb94e542151e2400
greenacre <bgree53@searshc.com>
2011-07-06 16:28:27 -0500

    Added in a new helper class to generate an img element for products.

---
+++ application/views/front/product/grid.php
@@ -0,0 +1,10 @@
+<ul>
+<?php if (count($products) > 0): ?>
+<?php while ($products->valid()): ?>
+<li class="shcp-item">
+<?php echo Helper_Products::image($products->image); ?>
+<?php echo $products->post_title; ?>
+</li>
+<?php $products->next(); endwhile; ?>
+<?php endif; ?>
+</ul>
