3f145204be7bc5ed5cd40b520a2f263c40292940
greenacre <bgree53@searshc.com>
2011-07-06 15:05:17 -0500

    Added a trash icon. Related product section is working for dragging and dropping. Also, removing related products works.

--- application/views/admin/related/grid.php
+++ application/views/admin/related/grid.php
@@ -2,10 +2,19 @@
 <?php foreach ($products as $product): ?>
 <li class="shcp_product" data-post_id="<?php echo $product->ID; ?>" id="post_id_<?php echo $product->ID; ?>">
 <?php if (isset($related_tank)): ?>
+<a href="#" class="shcp_trash"><img src="/wp-content/plugins/shcproducts/assets/images/trash.png" alt="Remove Product" height="22px" width="20px" /></a>
 <input type="hidden" name="shcp_related_products[]" value="<?php echo $product->ID; ?>" />
 <?php endif; ?>
 <img src="http://s.shld.net/is/image/Sears/<?php echo $products->imageid; ?>?hei=100&amp;wid=100" style="width: 100px;" alt="<?php echo $products->imageid; ?>" />
-<p><b><?php echo $product->post_title; ?>:</b> $<?php echo $products->displayprice; ?></p>
+<p>
+<b><?php echo $product->post_title; ?>:</b>
+<?php if ($product->cutprice): ?>
+<del>$<?php echo $products->cutprice; ?></del>
+<b>$<?php echo $products->displayprice; ?></b>
+<?php else: ?>
+$<?php echo $products->displayprice; ?>
+<?php endif; ?>
+</p>
 </li>
 <?php endforeach; ?>
 </ul>
