b209a8bce6098d42ffc626b93fb67b124eb755cf
greenacre <bgree53@searshc.com>
2011-07-06 15:30:20 -0500

    Updated the header doc blocks in all the php files.

--- application/classes/controller/admin/options.php
+++ application/classes/controller/admin/options.php
@@ -1,8 +1,12 @@
 <?php defined('SHCP_PATH') OR die('No direct script access.');
 /**
- * shcproducts
+ * Sears Holding Company Products Wordpress plugin.
+ *
+ * Provides the ability to import products via the Sears API and storing in
+ * wordpress as custom post type.
  *
  * @author Brian Greenacre and Kyla Klein
+ * @package shcproducts
  * @email bgreenacre42@gmail.com
  * @version $Id$
  * @since Wed 15 Jun 2011 07:32:09 PM
