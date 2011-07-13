6c97f2653b2fd520788b9a20e4ca0ee2d0c41b26
greenacre <bgree53@searshc.com>
2011-07-13 13:36:44 -0500

    Fixed widget forms not saving.

--- application/views/widget/products/form.php
+++ application/views/widget/products/form.php
@@ -1,12 +1,12 @@
 <p>
 <label for="<?php echo SHCP::get($ids, 'title'); ?>"><?php echo __('Title'); ?>:</label><br />
-<input type="text" name="title" value="<?php echo SHCP::get($values, 'title'); ?>" id="<?php echo SHCP::get($ids, 'title'); ?>" class="widefat" />
+<input type="text" name="<?php echo SHCP::get($names, 'title'); ?>" value="<?php echo SHCP::get($values, 'title'); ?>" id="<?php echo SHCP::get($ids, 'title'); ?>" class="widefat" />
 </p>
 <p>
 <label for="<?php echo SHCP::get($ids, 'keyword'); ?>"><?php echo __('Keyword'); ?>:</label><br />
-<input type="text" name="keyword" value="<?php echo SHCP::get($values, 'keyword'); ?>" id="<?php echo SHCP::get($ids, 'keyword'); ?>" class="widefat" />
+<input type="text" name="<?php echo SHCP::get($names, 'keyword'); ?>" value="<?php echo SHCP::get($values, 'keyword'); ?>" id="<?php echo SHCP::get($ids, 'keyword'); ?>" class="widefat" />
 </p>
 <p>
 <label for="<?php echo SHCP::get($ids, 'limit'); ?>"><?php echo __('Limit Products'); ?>:</label><br />
-<input type="text" name="limit" value="<?php echo SHCP::get($values, 'limit'); ?>" id="<?php echo SHCP::get($ids, 'limit'); ?>" class="widefat" />
+<input type="text" name="<?php echo SHCP::get($names, 'limit'); ?>" value="<?php echo SHCP::get($values, 'limit'); ?>" id="<?php echo SHCP::get($ids, 'limit'); ?>" class="widefat" />
 </p>
