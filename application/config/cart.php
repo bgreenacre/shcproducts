c4d7c922603b61a71d4c0df7e6276f8890c2a13b
greenacre <bgree53@searshc.com>
2011-07-15 12:34:21 -0500

    Built a basic view cart template.

---
+++ application/config/cart.php
@@ -0,0 +1,22 @@
+<?php defined('SHCP_PATH') OR die('No direct script access.');
+
+return array(
+    'defaults'  => array(
+        'items' => array(),
+        'discounts' => array(),
+        'item_count'    => 0,
+        'total_item_price'   => 0.00,
+        'total_discounts'   => 0.00,
+        'total_weight'  => 0.00,
+        'total_price'   => 0.00,
+        'session'       => NULL,
+        'order_id'      => NULL,
+        'coupons'       => array(),
+        'custom_fiels'  => array(),
+        'messages'      => array(
+            'information'   => array(),
+            'notices'       => array(),
+            'errors'        => array(),
+        ),
+    ),
+);
