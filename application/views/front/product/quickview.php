c7fe730a169103057bfd4a815be872f3743b5351
Kyla Klein <kylaklein@gmail.com>
2011-07-21 14:13:43 -0500

    [shcproducts] Kyla: some front end work on details and quickview

--- application/views/front/product/quickview.php
+++ application/views/front/product/quickview.php
@@ -1,21 +1,30 @@
 <form action="<?php echo bloginfo('url').'/cart/add?catentryid='.$product->get_catentryid(); ?>" method="GET" class="shcp-quickview" id="shcp_product-detail">
-<div class="shcp-image-tank">
-<div class="shcp-current-image"><?php echo Helper_Products::image($product->imageid, array('height' => '248', 'width' => '248')); ?></div>
-<?php if ($product->detail->imageurls): ?>
-<?php foreach ($product->detail->imageurls->imageurl[1] as $image): ?>
-<a href="#" class="shcp-image-thumbnail"><?php echo Helper_Products::image($image, array('height' => 30, 'width' => 30), TRUE); ?></a>
-<?php endforeach; ?>
-<?php endif; ?>
-</div>
-<p><a href="<?php echo get_permalink($product->ID); ?>">Go to Product Page</a></p>
-<h1><?php echo $product->post_title; ?></h1>
-<p class="shcp-item-shortdesc"><?php echo $product->detail->shortdescription; ?></p>
-<p class="shcp-item-price">
-<span>$<?php echo $product->detail->saleprice; ?></span>
-<?php if ($product->detail->regularprice): ?>
-<span class="price-savings">A savings of $<?php echo number_format(abs((float)($product->detail->saleprice - $product->detail->regularprice)), 2); ?></span>
-<?php endif; ?>
-</p>
-<a href="<?php echo bloginfo('url').'/cart/add?catentryid='.$product->get_catentryid(); ?>" class="addtocart">Add To Cart</a>
-<div class="shcp-item-longdesc"><?php echo $product->detail->longdescription; ?></div>
+  <div class="close_button"><a href="" id="shcp_quickview_close" class="close">Close</a></div>
+  <div class="quickview_wrapper">
+    <div class="shcp-image-tank">
+      <div class="shcp-current-image"><?php echo Helper_Products::image($product->imageid, array('height' => '248', 'width' => '248')); ?></div>
+  <?php if ($product->detail->imageurls): ?>
+      <div class="shcp-more-images">
+  <?php foreach ($product->detail->imageurls->imageurl[1] as $image): ?>
+        <a href="" class="shcp-image-thumbnail<?php if (substr($image, strrpos($image, '/') + 1) == $product->imageid): echo " selected"; endif; ?>">
+          <?php echo Helper_Products::image($image, array('height' => 40, 'width' => 40)); ?>
+        </a>
+  <?php endforeach; ?>
+      </div>
+  <?php endif; ?>
+    </div>
+    <h1><?php echo $product->post_title; ?></h1>
+    <div class="shcp-item-shortdesc"><?php echo htmlspecialchars_decode($product->detail->shortdescription); ?></div>
+    <p class="shcp-item-price">
+      <span>$<?php echo $product->detail->saleprice; ?></span>
+  <?php if ($product->detail->regularprice): 
+          $price_savings = number_format(abs((float)($product->detail->saleprice - $product->detail->regularprice)), 2);
+          if($price_savings != 0.00): ?>
+      <span class="price-savings">A savings of $<?php echo $price_savings; ?></span>
+          <?php endif; ?>
+  <?php endif; ?>
+    </p>
+    <p><a href="<?php echo bloginfo('url').'/cart/add?catentryid='.$product->get_catentryid(); ?>" class="addtocart">Add To Cart</a></p>  
+    <div class="shcp-item-longdesc"><?php echo htmlspecialchars_decode($product->detail->longdescription); ?></div>
+  </div>
 </form>
