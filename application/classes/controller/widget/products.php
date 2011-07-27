09da16dbce4e427ebef84e034fecc60b819e45be
greenacre <bgree53@searshc.com>
2011-07-27 13:07:36 -0500

    Got the Products widget to work the way I want.

--- application/classes/controller/widget/products.php
+++ application/classes/controller/widget/products.php
@@ -21,6 +21,7 @@ class Controller_Widget_Products extends SHCP_Controller_Widget {
      * @var string
      */
     protected $content = 'widget/products/list';
+    protected $cache = FALSE;
 
     public function form($values = NULL)
     {
@@ -29,11 +30,13 @@ class Controller_Widget_Products extends SHCP_Controller_Widget {
                 'title'     => $this->get_field_id('title'),
                 'keyword'   => $this->get_field_id('keyword'),
                 'limit'     => $this->get_field_id('limit'),
+                'randomize' => $this->get_field_id('randomize'),
             ),
             'names'       => array(
                 'title'     => $this->get_field_name('title'),
                 'keyword'   => $this->get_field_name('keyword'),
                 'limit'     => $this->get_field_name('limit'),
+                'randomize' => $this->get_field_name('randomize'),
             ),
             'values'    => array_merge(SHCP::config('widget/products.options'), $values),
         );
@@ -41,27 +44,80 @@ class Controller_Widget_Products extends SHCP_Controller_Widget {
         echo SHCP::view('widget/products/form', $data);
     }
 
+    public function widget($args, $instance)
+    {
+        $products = new Model_Products();
+        
+        $products
+            ->param('post__in', $instance['pool'])
+            ->orderby((SHCP::get($instance, 'randomize')) ? 'rand': 'none')
+            ->limit((int) SHCP::get($instance, 'limit', 3))
+            ->load();
+        
+        $data = array(
+            'products'  => $products,
+        );
+        
+        $this->content = SHCP::view($this->content, $data);
+        
+        parent::widget($args, $instance);
+    }
+    
     public function update($new, $old)
     {
         $data = array_merge($old, $new);
         $data['title'] = strip_tags($data['title']);
+        $data['pool'] = (array) SHCP::get($data, 'pool');
 
         $search = Library_Sears_Api::factory('search')
             ->keyword(SHCP::get($data, 'keyword'));
 
+        if ($randomize = (bool) SHCP::get($data, 'randomize'))
+        {
+            $limit = ((int) SHCP::get($data, 'limit', 3)) * 20;
+            $search->limit(0, $limit);
+        }
+        
+        $search->load();
+        
         if (count($search) > 0)
         {
-            $search->reset();
+            $search->rewind();
 
             while ($search->valid())
             {
-                $product = new Model_Product();
+                $product = new Model_Products();
 
-                $product->param('partnumber', $search->partNumber);
+                $product->meta('partnumber', '=', $search->partnumber);
 
                 if ( ! $product->loaded())
                 {
-
+                    echo $search->name;
+                    $data_to_import = array(
+                        'post_title'    => $search->name,
+                        'imageid'       => $search->imageid,
+                        'numreview'     => $search->numreview,
+                        'catentryid'    => $search->catentryid,
+                        'rating'        => $search->rating,
+                        'partnumber'    => $search->partnumber,
+                        'cutprice'      => $search->cutprice,
+                        'displayprice'  => $search->displayprice,
+                    );
+                    
+                    $data_to_import['detail'] = Library_Sears_Api::factory('product')
+                        ->get($data_to_import['partnumber'])
+                        ->load();
+                    
+                    $product->values($data_to_import);
+                    
+                    if ($product->check())
+                    {
+                        $product->save();
+                        $data['pool'][] = $product->ID;
+                    }
+                    else
+                    {
+                    }
                 }
 
                 $search->next();
