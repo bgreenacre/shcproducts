5204e412d2d4e2eca77cbadaaee2f104dd3375a4
Dan Crimmins <dbcrimmins@gmail.com>
2012-10-29 14:13:59 -0500

    Changed from e-mail address for cron e-mail.

--- application/classes/controller/crons/products.php
+++ application/classes/controller/crons/products.php
@@ -349,7 +349,7 @@ class Controller_Crons_Products {
      */
     protected function mail_report() {
     	
-    	$to = 'dcrimmi@searshc.com';//'phpteam@searshc.com';
+    	$to = 'phpteam@searshc.com';
     	$subject = 'SHC Products Update for ' . $this->_blog_name;
     	$body = ($this->_activity_log) ? "Product updates for " . $this->_blog_name . ": \n Cron job completed on: " . date('m-d-Y H:i:s') . "\n Products Updated: {$this->_num_updated} \n Products Set to Draft: {$this->_num_deleted} \n Status: ". $this->_status ."\n\n" .  $this->log_to_string($this->_activity_log) : 'No products were updated or set to draft.';
     	
