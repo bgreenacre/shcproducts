5ea045f620340478839d3ba92f3c5806fdfcffae
Brian Greenacre <bgreenacre42@gmail.com>
2016-01-02 20:58:44 -0600

    Adding default config for api.

---
+++ application/config/api.php
@@ -0,0 +1,13 @@
+<?php defined('SHCP_PATH') or die('No direct script access.');
+
+return array(
+    'default'   => array(
+        'apikey'    => 'changeme',
+        'authid'    => 'changeme',
+        'appid'     => 'changeme',
+        'endpoint'  => 'http://webservices.sears.com/shcapi/',
+        'store'     => 'Sears',
+        'cache'     => TRUE,
+        'forceupdate' => FALSE
+    ),
+);
